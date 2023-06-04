189f739da327ab375e7ab6f4a87ded8456913763
Alexander Moreira <olivermoreira16@gmail.com>
2023-06-04 12:22:12 -0500

    Add test api and e2e in Jenkinsfile

---
+++ Jenkinsfile.actividad.groovy
@@ -0,0 +1,42 @@
+pipeline {
+    agent {
+        label 'docker'
+    }
+    stages {
+        stage('Source') {
+            steps {
+                git 'https://github.com/oliberto16/unir-cicd.git'
+            }
+        }
+        stage('Build') {
+            steps {
+                echo 'Building stage!'
+                sh 'make build'
+            }
+        }
+        stage('Unit tests') {
+            steps {
+                sh 'make test-unit'
+                archiveArtifacts artifacts: 'results/*.xml'
+            }
+        }
+        stage('Api tests') {
+            steps {
+                sh 'make test-api'
+                archiveArtifacts artifacts: 'results/*.xml'
+            }
+        }
+        stage('E2E tests') {
+            steps {
+                sh 'make test-e2e'
+                archiveArtifacts artifacts: 'results/*.xml'
+            }
+        }
+    }
+    post {
+        always {
+            junit 'results/*_result.xml'
+            cleanWs()
+        }
+    }
+}
