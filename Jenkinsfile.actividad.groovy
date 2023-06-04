09e4f2d6fc3eacead580c786c9e3286398dee2e5
Alexander Moreira <olivermoreira16@gmail.com>
2023-06-04 14:39:11 -0500

    Update Jenkinsfile v3

--- Jenkinsfile.actividad.groovy
+++ Jenkinsfile.actividad.groovy
@@ -14,36 +14,50 @@ pipeline {
                     try {
                         echo 'Building stage!'
                         sh 'make build'
-                        def jobName = env.JOB_NAME
-                        def buildNumber = env.BUILD_NUMBER
-                        //Simulate send mail with echo in Console
-                        echo "to: example@maildestino.com"
-                        echo "subject: Job ${jobName} failed - Execution #${buildNumber}"
-                        echo "body: A pipeline failure has occurred. See the Jenkins logs for more details: http://localhost:8080/job/${jobName}/${buildNumber}/testReport"
                     } catch (Exception e) {
                         currentBuild.result = 'FAILURE'
                         error("Error en Stage Build: ${e.message}")
                     }
                 }
-                
             }
         }
         stage('Unit tests') {
             steps {
-                sh 'make test-unit'
-                archiveArtifacts artifacts: 'results/*.xml'
+                script {
+                    try {
+                        sh 'make test-unit'
+                        archiveArtifacts artifacts: 'results/*.xml'
+                    } catch (Exception e) {
+                        currentBuild.result = 'FAILURE'
+                        error("Error en Stage Build: ${e.message}")
+                    }
+                }
             }
         }
         stage('Api tests') {
             steps {
-                sh 'make test-api'
-                archiveArtifacts artifacts: 'results/*.xml'
+                script {
+                    try {
+                        sh 'make test-api'
+                        archiveArtifacts artifacts: 'results/*.xml'
+                    } catch (Exception e) {
+                        currentBuild.result = 'FAILURE'
+                        error("Error en Stage Build: ${e.message}")
+                    }
+                }
             }
         }
         stage('E2E tests') {
             steps {
-                sh 'make test-e2e'
-                archiveArtifacts artifacts: 'results/*.xml'
+                script {
+                    try {
+                        sh 'make test-e2e'
+                        archiveArtifacts artifacts: 'results/*.xml'
+                    } catch (Exception e) {
+                        currentBuild.result = 'FAILURE'
+                        error("Error en Stage Build: ${e.message}")
+                    }
+                }
             }
         }
     }
@@ -53,21 +67,17 @@ pipeline {
             cleanWs()
         }
         failure {
-            // Get Job_name and Build_number
-            def jobName = env.JOB_NAME
-            def buildNumber = env.BUILD_NUMBER
-
             // Send mail with Email Extension Plugin (Configure SMTP Jenkins)
-            emailext (
+            /*emailext (
                 to: 'example@maildestino.com',
-                subject: "Job ${jobName} failed - Execution #${buildNumber}",
-                body: "A pipeline failure has occurred. See the Jenkins logs for more details: http://localhost:8080/job/${jobName}/${buildNumber}/testReport"
-            )
+                subject: "Job ${env.JOB_NAME} failed - Execution #${env.BUILD_NUMBER}",
+                body: "A pipeline failure has occurred. See the Jenkins logs for more details: ${env.JENKINS_URL}/job/${env.JOB_NAME}/${env.BUILD_NUMBER}/testReport"
+            )*/
 
             //Simulate send mail with echo in Console
             echo "to: example@maildestino.com"
-            echo "subject: Job ${jobName} failed - Execution #${buildNumber}"
-            echo "body: A pipeline failure has occurred. See the Jenkins logs for more details: http://localhost:8080/job/${jobName}/${buildNumber}/testReport"
+            echo "subject: Job ${env.JOB_NAME} failed - Execution #${env.BUILD_NUMBER}"
+            echo "body: A pipeline failure has occurred. See the Jenkins logs for more details: ${env.JENKINS_URL}/job/${env.JOB_NAME}/${env.BUILD_NUMBER}/testReport"
         }
     }
 }
